62508d15ba6bc3dba930513b3d4a1f3e022ab307
Albert Wales <41163950+AlbertBW@users.noreply.github.com>
2023-10-03 21:04:10 +0100

    Almost complete shopping cart

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import { Container, Nav } from "react-bootstrap";
+import { Home } from "./pages/Home.tsx";
+import { Store } from "./pages/Store.tsx";
+import { About } from "./pages/About.tsx";
+import { Navbar } from "./components/Navbar.tsx";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext.tsx";
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
